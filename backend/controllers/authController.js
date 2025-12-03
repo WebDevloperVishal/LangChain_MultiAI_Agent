e8f7268717ed622b35c23276025f5700337dd756
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-12-03 18:04:42 +0530

    first

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,7 @@
 export const RegisterController = async (req ,res) => {
-    
+    try {
+        
+    } catch (error) {
+        
+    }
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,12 @@
 import express from "express";
 import AuthRouter from "./authRoutes.js";
+import { PrismaClient } from "@prisma/Client"
+
+export const prisma = new PrismaClient({
+    log: ["query"],
+})
+
+
 
 const rootRouter = express.Router();
 
