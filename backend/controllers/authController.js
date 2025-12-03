38b0172ba2733e530ae6a9767cc91a1de931e69e
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-12-03 19:51:56 +0530

    first

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,30 @@
+import { email } from "zod";
+import { RegisterSchema } from "../schema/user.js"
+
 export const RegisterController = async (req ,res) => {
     try {
-        
+        const userSchema = RegisterSchema.parse(req.body);
+        console.log(userSchema);
+
+        if(!userSchema.name || userSchema.email || userSchema.password){
+             return res.status(400).json({
+                success: false,
+                message: "Please fill all the fields"
+            })
+        }
+
+        const existInUser = await WebGLShaderPrecisionFormat.user.findUnique({
+            where:{
+                email:userSchema.email
+            }
+        })
+
+         if(existingUser){
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            })
+        }
     } catch (error) {
         
     }
